2eb85eb8d5e46860a75b7dc8a350199a5c03be14
7Vanessa <vmohammedi@outlook.fr>
2023-11-08 14:54:40 +0100

    test sqrt fail

--- src/test/java/ReversePolishNotationTest.java
+++ src/test/java/ReversePolishNotationTest.java
@@ -8,7 +8,13 @@ import org.junit.Test;
 public class ReversePolishNotationTest {
 
     @Test
-    public void testSqrt() throws IllegalArgumentException {
+    public void testSqrtFail() throws IllegalArgumentException {
+        Exception exception = Assert.assertThrows(IllegalArgumentException.class, () -> {evaluateRPN("sqrt");});
+        assertTrue(exception.getMessage().contains("Error: expecting one argument for `sqrt`, but got less than one argument"));
+    }
+
+    @Test
+    public void testSqrtOK() throws IllegalArgumentException {
         Assert.assertEquals(3.0, evaluateRPN("9 sqrt"), 0.0);
     }
 
